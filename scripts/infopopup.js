a50ae629729e7b846c452cb38d3526ed44252165
BlakkM9 <blakkm92@gmail.com>
2019-01-19 07:29:25 +0100

    Added tooltips for buttons

--- scripts/infopopup.js
+++ scripts/infopopup.js
@@ -12,9 +12,12 @@ const INFO_TEXT = {
     gaps:       "Vertical and horizontal gap between the SpeedDials.</br>" +
                 "0 means no gap. Values between 0 and 100 are working good",
 
-    generate:   "",
+    generate:   "Generate a screenshot from the entered website and upload it to imgur.</br>" +
+                "This may take some while because the screenshot needs to be generated.",
 
-    browse:     ""
+    browse:     "Browse images from your computer. If you select an image it is automatically " +
+                "uploaded to imgur, so don't choose files which contain private data! (even if the " +
+                "imgur link is not public."
 };
 
 let infoPopup;
@@ -23,21 +26,37 @@ $(document).ready(function() {
     infoPopup = $("#info-popup");
 
     let info = $(".hover.info");
+    let infoText;
 
-    info.hover(function(e) {
-    }, function() {
+    info.hover(() => {}, function() {
         infoPopup.css("display", "");
     });
 
     info.mousemove(function(e) {
-        let id = e.target.parentElement.id;
-        if (!(id.length < 5)) {
-            id = id.substr(5, id.length);
+        let id;
+        if (e.target.tagName === "BUTTON" || e.target.tagName === "LABEL") {
+            id = e.target.id;
+            if (id.endsWith("-button")) {
+                let buttonStart = id.indexOf("-button");
+                id = id.substr(0, buttonStart);
+                if (INFO_TEXT.hasOwnProperty(id)) {
+                    infoText = INFO_TEXT[id];
+                } else {
+                    console.log("invalid button tooltip", id);
+                }
+            }
         } else {
-            id = "invalid tooltip";
-        }
+            id = e.target.parentElement.id;
 
-        let infoText = INFO_TEXT[id];
+            if (id.startsWith("info-")) {
+                id = id.substr(5, id.length);
+                if (INFO_TEXT.hasOwnProperty(id)) {
+                    infoText = INFO_TEXT[id];
+                } else {
+                    console.log("invalid info tooltip", id);
+                }
+            }
+        }
 
         infoPopup.html(infoText);
 
